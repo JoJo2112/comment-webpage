d82b385b5dcd38c0a5c9988b72f2dee562a729b9
Jonathan Rudolph <jonathanr@hotmail.de>
2022-05-16 21:46:00 +0200

    first distribution diagrams

---
+++ src/routes/api/commentDistribution.js
@@ -0,0 +1,27 @@
+import db from '$lib/db';
+
+export async function get() {
+  try {
+    const count = await db.query(
+      'SELECT platforms.name, COUNT(*) FROM comments_platform_links, platforms WHERE comments_platform_links.platform_id = platforms.id GROUP BY name'
+    );
+
+    let sorted = count.rows.sort((x, y) => {
+      if (x.name < y.name) return -1;
+      if (x.name > y.name) return 1;
+      return 0;
+    });
+
+    return {
+      body: {
+        newspapers: sorted,
+      },
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      body: 'An error occured',
+      status: 400,
+    };
+  }
+}
