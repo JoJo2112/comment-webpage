89b5a9ef907c8b46bf8fe52a523c04e8f457dc66
Jonathan Rudolph <jonathanr@hotmail.de>
2022-05-17 13:22:26 +0200

    debug

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
-import postgres from 'postgres';
 
-let _sql = postgres();
+let pg;
+let _sql;
 
 async function sql() {
-  if (!_sql) _sql = postgres();
+  if (!_sql) {
+    pg = (await import('postgres')).default;
+  }
+  _sql = pg();
   const start = Date.now();
   const res = await _sql.apply(_sql, arguments);
   console.log('executed query', {
