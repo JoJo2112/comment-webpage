217aa914a8878e944172580629aeb80ec0709292
Jonathan Rudolph <jonathanr@hotmail.de>
2022-05-17 02:35:38 +0200

    new try

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,22 +1,12 @@
 import 'dotenv/config';
+import { Pool } from 'pg/lib';
 
-const createPool = async () => {
-  try {
-    const pg = (await import('pg')).default;
-    const { Pool } = pg;
-    console.log('new Pool created.');
-    return new Pool();
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const pool = createPool();
+const pool = new Pool();
 
 const db = {
   query: async (text, params) => {
     const start = Date.now();
-    const res = await (await pool).query(text, params);
+    const res = await pool.query(text, params);
     const duration = Date.now() - start;
     console.log('executed query', { text, duration, rows: res.rowCount });
     return res;
