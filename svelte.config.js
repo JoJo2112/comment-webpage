0629308115ef9fd2c98d95ff962447cb2e72d16c
Jonathan Rudolph <jonathanr@hotmail.de>
2022-05-17 09:59:56 +0200

    debug

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ export default {
 
       // an array of dependencies that esbuild should treat
       // as external when bundling functions
-      external: ['pg-native'],
+      external: ['pg'],
 
       // if true, will split your app into multiple functions
       // instead of creating a single one for the entire app
