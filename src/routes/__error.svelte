bc0bd283d22727fce1e7530bcdd11bda0534eabe
Jonathan Rudolph <jonathanr@hotmail.de>
2022-05-19 01:19:29 +0200

    error pag

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').Load} */
+  export function load({ error, status }) {
+    return {
+      props: {
+        status,
+        error: error.message,
+      },
+    };
+  }
+</script>
+
+<script>
+  import '../app.css';
+  export let status;
+  export let error;
+</script>
+
+<div class="flex flex-col h-full justify-center items-center">
+  <div class="flex items-center">
+    <h1 class="p-3 border-r text-3xl">{status}</h1>
+    <div class="p-3">
+      <h1 class="text-xl font-light opacity-90">{error}</h1>
+    </div>
+  </div>
+</div>
